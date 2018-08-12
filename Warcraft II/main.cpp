39b64cfbdf7d24ab666229036b23afffb75dc0ba
Szoke Lorand <lori.szoke@yahoo.com>
2018-08-13 01:47:01 +0300

    Solution created.

---
+++ Warcraft II/main.cpp
@@ -0,0 +1,28 @@
+#include "SFML\Graphics.hpp"
+
+int main()
+{
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "SFML");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			//insert input handling in here
+
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		//insert drawing commands in here
+
+		window.display();
+	}
+
+	return 0;
+}
